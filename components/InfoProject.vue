d72886e2e0e6838f936d6124ec75a392f8935670
Edvin Nordin <nordin.edvin@gmail.com>
2025-05-29 01:01:46 +0200

    button links

--- components/InfoProject.vue
+++ components/InfoProject.vue
@@ -23,6 +23,14 @@ function formatName(name) {
         <div class="description">
           <p class="subtitle">{{ project.description }}</p>
         </div>
+        <div class="w-1/5 text-center inline-block">
+          <a :href="project.link" target="_blank">
+            <div class="butt">Link</div>
+          </a>
+          <a :href="project.repo" target="_blank">
+            <div class="butt">Github</div>
+          </a>
+        </div>
       </div>
       <img :src="project.image" :alt="project.name" class="image" />
     </div>
@@ -30,6 +38,12 @@ function formatName(name) {
 </template>
 
 <style scoped lang="scss">
+.butt {
+  background-color: grey;
+  border-radius: 5px;
+  padding: 10px;
+}
+
 .image {
   max-width: 700px;
   margin-left: 20px;
