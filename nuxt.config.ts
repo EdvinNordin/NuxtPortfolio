90c1a68604a4785e4cdfc581452cdf205a624d15
Edvin Nordin <nordin.edvin@gmail.com>
2025-06-04 15:05:16 +0200

    accidentaly removed nuxt config file

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  /* experimental: {
+    watcher: "chokidar",
+  }, */
+  compatibilityDate: "2025-05-28",
+  devtools: { enabled: false },
+  modules: ["@nuxt/eslint", "shadcn-nuxt"],
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+});
