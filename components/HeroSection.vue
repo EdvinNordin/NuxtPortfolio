8621892f7f5687d7ba320b06161a853ca19d3f1b
Edvin Nordin <nordin.edvin@gmail.com>
2025-06-07 14:19:49 +0200

    image cleaning

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -2,13 +2,15 @@
 
 <template>
   <div
-    class="flex flex-col md:flex-row w-7/8 md:w-3/4 mx-auto my-50 md:my-80 justify-around gap-30"
+    class="flex flex-col lg:flex-row w-7/8 lg:w-3/4 mx-auto my-50 lg:my-80 justify-around gap-30"
   >
-    <div class="flex flex-col items-center">
-      <h1 class="text-6xl primary font-bold">Edvin Nordin</h1>
+    <div class="flex flex-col lg:items-center">
+      <h1 class="text-6xl primary font-bold">
+        Edvin <br class="block lg:hidden" />Nordin
+      </h1>
       <h1 class="text-2xl">Your next developer?</h1>
     </div>
-    <div class="md:w-2/5 items-center">
+    <div class="lg:w-2/5 items-center">
       <!-- <h1 class="text-xl">Site is currently under construction!</h1> -->
       <p class="text-base items-center">
         Welcome to my website! I'm a 26-year-old passionate software engineer
