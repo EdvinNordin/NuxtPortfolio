e567a947e0522e2dafc245d872b95473b8fb59eb
edvno177 <nordin.edvin@gmail.com>
2025-06-13 15:58:15 +0200

    back and front same again

---
+++ backend/Program.cs
@@ -0,0 +1,52 @@
+
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowFrontend",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000", "http://www.edvinnordin.me")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                      });
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowFrontend");
+
+app.Urls.Add("http://localhost:5175");
+
+async Task<object> json()
+{
+    var json = await File.ReadAllTextAsync("projects.json");
+    return JsonSerializer.Deserialize<object>(json);
+}
+
+app.MapGet("/projects", async () =>
+{
+    var json = await File.ReadAllTextAsync("projects.json");
+    var projects = JsonSerializer.Deserialize<object>(json);
+    return projects;
+    //return json();
+});
+
+app.MapGet("/projects/{name}", async (string name) =>
+{
+
+    var projects = await json();
+    foreach (var project in ((JsonElement)projects).EnumerateArray())
+    {
+        if (project.GetProperty("name").GetString() == name)
+        {
+            return Results.Ok(project);
+        }
+    }
+    return Results.NotFound();
+});
+
+
+app.Run();
