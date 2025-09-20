204d66de229f5bd02c219ba7f59c857c41b17825
Edvin <nordin.edvin@gmail.com>
2025-09-20 11:24:36 +0200

    sentry test

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,20 +5,37 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true
   },
+
   css: ["~/assets/css/tailwind.css"],
   ssr: false,
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   /* experimental: {
     watcher: "chokidar",
   }, */
   compatibilityDate: "2025-05-28",
+
   devtools: { enabled: false },
-  modules: ["@nuxt/eslint", "shadcn-nuxt",],
+  modules: ["@nuxt/eslint", "shadcn-nuxt", "@sentry/nuxt/module"],
 
   shadcn: {
     prefix: "",
     componentDir: "./components/ui",
+  },
+
+  sentry: {
+    sourceMapsUploadOptions: {
+      org: "innord",
+      project: "nuxtportfolio",
+    },
+
+    autoInjectServerSentry: "top-level-import",
+  },
+
+  sourcemap: {
+    client: "hidden",
   },
 });
